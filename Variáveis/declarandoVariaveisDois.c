ecd1de3865abd8aea3309dee121795cc86924be1
Ueverton Passos <uevertonpassos96@gmail.com>
2021-11-26 07:53:54 -0300

    falhei miseravelmente

--- Variáveis/declarandoVariaveisDois.c
+++ Variáveis/declarandoVariaveisDois.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main(){
-	int a = 5, b;
+	int a = 5, b = a + a;
 	printf("A soma equivale a: %d", a);
-	a = 15;
-	printf(" e o outro equivale a %d", b);
+	printf(" somando: %d + %d = &b", b);
 	return 0;
 }
